2d82d8e715285dbd2d8ba8a89ca161689b5d4304
Diego <otero.diego.87@gmail.com>
2024-07-18 00:26:59 -0300

    feat: initial scaffolding

---
+++ src/main/java/ar/com/dno/ai/rag/AiRagApplication.java
@@ -0,0 +1,15 @@
+package ar.com.dno.ai.rag;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AiRagApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AiRagApplication.class, args);
+    }
+
+}
